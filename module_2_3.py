f8d4a3583b33973a894cd7fed4a2fde793083a0a
S-H-O-K-E-R <kolyan482650@gmail.com>
2024-08-26 20:03:42 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,6 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
-while i < len(set(my_list)):
+while i < len(my_list):
     if my_list[i] >= 1:
         print(my_list[i])
         i = i + 1
